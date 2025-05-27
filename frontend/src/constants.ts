902afcfbb9b1ee6aab4b90f2174bd7a7d87078de
Adrian Ceniza <adriancenizadev@gmail.com>
2025-05-27 19:01:18 +0100

    fix: update WebSocket URL to use NEXT_PUBLIC environment variable

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,5 @@
-export const WS_URL = process.env.WS_URL || "ws://localhost:8080/ws";
+export const WS_URL =
+  process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080/ws";
 
 export enum WSIncomingMessageType {
   JOINED_SESSION = "joined_session",
