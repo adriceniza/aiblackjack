3ae6df6cb96aff2978289469fc95d1212f88e9f8
Adrian Ceniza <adriancenizadev@gmail.com>
2025-05-28 00:53:24 +0100

    fix: pass playerName to joinSession in JoinByCodeForm

--- frontend/src/components/forms/JoinByCodeForm/JoinByCodeForm.tsx
+++ frontend/src/components/forms/JoinByCodeForm/JoinByCodeForm.tsx
@@ -3,20 +3,22 @@ import styles from './JoinByCodeForm.module.css'
 import CodeField from '../CodeField/CodeField'
 import { useGame } from '@/context/GameContext';
 
-export default function JoinByCodeForm() {
-    const { playerName, joinSession } = useGame();
-    const [code, setCode] = useState("");
+export default function JoinByCodeForm(playerName: string) {
+  const { joinSession } = useGame();
+  const [code, setCode] = useState("");
 
-    const handleOnClick = () => {
-        joinSession(code);
-    }
+  const handleOnClick = () => {
+    joinSession(code, playerName);
+  };
 
-    return (
-        <div className={styles.joinByCodeForm}>
-            <CodeField onChange={setCode} />
-            {
-                (playerName && code.length === 5) && <button className={styles.button} onClick={handleOnClick}>Entrar</button>
-            }
-        </div>
-    )
+  return (
+    <div className={styles.joinByCodeForm}>
+      <CodeField onChange={setCode} />
+      {playerName && code.length === 5 && (
+        <button className={styles.button} onClick={handleOnClick}>
+          Entrar
+        </button>
+      )}
+    </div>
+  );
 }
