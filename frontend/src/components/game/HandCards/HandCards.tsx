2bd3ee6592fe60e54ec527e731ccb1408be6abb2
Adrian Ceniza <adriancenizadev@gmail.com>
2025-05-28 01:03:22 +0100

    fix: update player type to PlayerDTO in HandCards component

--- frontend/src/components/game/HandCards/HandCards.tsx
+++ frontend/src/components/game/HandCards/HandCards.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import styles from './HandCards.module.css'
-import { Player } from "../screens/GameTableScreen/GameTableScreen";
+import styles from "./HandCards.module.css";
 import Card from '../Card/Card';
+import { PlayerDTO } from "@/types/player";
 
-export default function HandCards({ player }: { player: Player }) {
-
-  return <div className={styles.handContainer}>
-    {player.hand.map((card, index) => {
-      if (card === "??") {
-        return <Card key={index} card={'back'} />
-      }
-      return <Card key={index} card={card} />
-    })}
-  </div>
+export default function HandCards({ player }: { player: PlayerDTO }) {
+  return (
+    <div className={styles.handContainer}>
+      {player.hand.map((card, index) => {
+        if (card === "??") {
+          return <Card key={index} card={"back"} />;
+        }
+        return <Card key={index} card={card} />;
+      })}
+    </div>
+  );
 }
